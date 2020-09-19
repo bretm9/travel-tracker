2310e4127d9a4b502fbd373a66ec8d6c5a67e4b3
Bret Merritt <merrittbret9@gmail.com>
2020-09-18 19:58:02 -0600

    Add getTravelerDestinations

--- src/fetch.js
+++ src/fetch.js
@@ -5,6 +5,17 @@ let allDestinations;
 
 let fetcher = {
 
+  getTravelerDestinations(traveler, userID) {
+    // this.getAllDestinations(allDestinations)
+    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations')
+      .then(response => response.json())
+      .then(data => allDestinations = data.destinations)
+      .then(() => {
+        this.getTraveler(traveler, userID);
+      })
+      .catch(err => console.log('Fetch error: ', err))
+  },
+
   getTraveler(traveler) {
     fetch(`https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/1`)
       .then(response => response.json())
