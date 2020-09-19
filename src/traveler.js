44b3a2a01b7b40ad10c77c0db8f76cb2f5264dd2
Bret Merritt <merrittbret9@gmail.com>
2020-09-19 13:22:17 -0600

    Add get totalSpentThisYear

--- src/traveler.js
+++ src/traveler.js
@@ -4,6 +4,23 @@ class Traveler {
     this.name = travelerData.name;
     this.travelerType = travelerData.travelerType;
   }
+
+  getTotalSpentThisYear() {
+    this.tripsThisYear = []
+    this.trips.forEach(trip => {
+      if (trip.date.includes('2020')) {
+        this.tripsThisYear.push(trip);
+      }
+    });
+    return this.tripsThisYear.reduce((totalSpent, currentTrip) => {
+      let currentDestination = this.allDestinations.find(destination => {
+        return destination.id === currentTrip.destinationID;
+      });
+      let totalFlightCost = currentTrip.travelers * currentDestination.estimatedFlightCostPerPerson;
+      let totalLodgingCost = currentTrip.duration * currentDestination.estimatedLodgingCostPerDay;
+      return totalSpent + (totalFlightCost + totalLodgingCost);
+    }, 0);
+  }
 }
 
 export default Traveler;
