7a634d7f5c641c3234d49bd0360808896633daa7
Bret Merritt <merrittbret9@gmail.com>
2020-09-19 14:01:30 -0600

    Fix testing

--- test/fetch-test.js
+++ test/fetch-test.js
@@ -1,36 +1,47 @@
 const chai = require('chai');
 const expect = chai.expect;
 const spies = require('chai-spies');
-import fetch from '../src/fetch';
+import fetcher from '../src/fetch';
 
 chai.use(spies);
 
 describe('fetch', () => {
   beforeEach(() => {
-    chai.spy.on(fetch, ['getTraveler'], () => true)
+    chai.spy.on(fetcher, [
+      'getTravelerDestinations',
+      'getTraveler',
+      'getUserTrips',
+      'getAllDestinations',
+      'renderAfterFetch'
+    ], () => true)
   });
   
   afterEach(() => {
-    chai.spy.restore(fetch);
+    chai.spy.restore(fetcher);
   });
 
   it('should be able to get a specific traveler\'s destinations', () => {
-    fetch.getTravelerDestinations();
-    expect(fetch.getTravelerDestinations).to.have.been.called(1);
+    fetcher.getTravelerDestinations();
+    expect(fetcher.getTravelerDestinations).to.have.been.called(1);
   });
 
   it('should be able to get a traveler and instantiate a new Traveler', () => {
-    fetch.getTraveler();
-    expect(fetch.getTraveler).to.have.been.called(1);
+    fetcher.getTraveler();
+    expect(fetcher.getTraveler).to.have.been.called(1);
   });
 
   it('should be able to get a travelers trips and set them to a traveler\'s "trips" property', () => {
-    fetch.getUserTrips();
-    expect(fetch.getUserTrips).to.have.been.called(1);
+    fetcher.getUserTrips();
+    expect(fetcher.getUserTrips).to.have.been.called(1);
   });
 
   it('should be able to get all destinations', () => {
-    fetch.getAllDestinations();
-    expect(fetch.getAllDestinations).to.have.been.called(1);
+    fetcher.getAllDestinations();
+    expect(fetcher.getAllDestinations).to.have.been.called(1);
+  });
+
+  it('should invoke domUpdates renders after fetching', () => {
+    fetcher.renderAfterFetch();
+    expect(fetcher.renderAfterFetch).to.have.been.called(1);
   });
 });
--- test/traveler-test.js
+++ test/traveler-test.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 
 import Traveler from '../src/traveler';
 
-let traveler1Data, traveler1;
+let traveler1Data, destinations, traveler1;
 
 describe('Traveler', () => {
   beforeEach(() => {
@@ -14,6 +14,47 @@ describe('Traveler', () => {
     }
 
     traveler1 = new Traveler(traveler1Data);
+    traveler1.trips = [
+      {
+        "id": 1,
+        "userID": 1,
+        "destinationID": 1,
+        "travelers": 1,
+        "date": "2020/09/16",
+        "duration": 1,
+        "status": "approved",
+        "suggestedActivities": []
+      },
+      {
+        "id": 2,
+        "userID": 1,
+        "destinationID": 2,
+        "travelers": 2,
+        "date": "2020/05/01",
+        "duration": 2,
+        "status": "approved",
+        "suggestedActivities": []
+      }
+    ];
+
+    traveler1.allDestinations = [
+      {
+        "id": 1,
+        "destination": "Lima, Peru",
+        "estimatedLodgingCostPerDay": 1,
+        "estimatedFlightCostPerPerson": 5,
+        "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+        "alt": "overview of city buildings with a clear sky"
+      },
+      {
+        "id": 2,
+        "destination": "Stockholm, Sweden",
+        "estimatedLodgingCostPerDay": 2,
+        "estimatedFlightCostPerPerson": 10,
+        "image": "https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+        "alt": "city with boats on the water during the day time"
+      }
+    ]
   });
 
   it('should be a function', () => {
@@ -34,5 +75,9 @@ describe('Traveler', () => {
 
   it('should have a property of travelerType', () => {
     expect(traveler1.travelerType).to.equal('relaxer');
+  });
+
+  it('should be able to get total money spent this year', () => {
+    expect(traveler1.getTotalSpentThisYear()).to.equal(30);
   });
 });
