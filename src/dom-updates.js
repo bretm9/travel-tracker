f84e0b87bbd4a6043b69118f01bacf1d0993e4e8
Bret Merritt <merrittbret9@gmail.com>
2020-09-19 13:18:38 -0600

    Add renderTotalSpentThisYear

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -1,4 +1,4 @@
-import bookedTripsBody from './index'
+import nodes from './index'
 
 let domUpdates = {
 
@@ -10,13 +10,21 @@ let domUpdates = {
 
   renderUserTrips(traveler, allDestinations) {
     traveler.trips.forEach(trip => {
-      bookedTripsBody.insertAdjacentHTML(`beforeend`, 
-        `<p>Destination: ${allDestinations[trip.destinationID - 1].destination}</p>
+      nodes.bookedTripsBody.insertAdjacentHTML(`beforeend`, 
+      `<article class="section-sub-card" id="booked-trips-sub-card"></article>
+        <p>Destination: ${allDestinations[trip.destinationID - 1].destination}</p>
         <p>Travelers: ${trip.travelers}</p>
         <p>Date: ${trip.date}</p>
-        <p>Status: ${trip.status}</p>`
+        <p>Status: ${trip.status}</p>
+      </article>`
       );
     });
+  },
+
+  renderTotalSpentThisYear(traveler) {
+    nodes.totalSpentBody.insertAdjacentHTML('beforeend', 
+      `<h3>You have spent $${traveler.getTotalSpentThisYear()} this year.</h3>`
+      )
   }
 }
 
