3224b531360e4a6b613028886e25401f07d49e7e
Bret Merritt <merrittbret9@gmail.com>
2020-09-18 19:59:47 -0600

    Add getAllDestinations

--- src/fetch.js
+++ src/fetch.js
@@ -41,6 +41,13 @@ let fetcher = {
         domUpdates.renderUserTrips(traveler, allDestinations)
       })
       .catch(err => console.log('Fetch error: ', err))
+  },
+
+  getAllDestinations(allDestinations) {
+    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations')
+      .then(response => response.json())
+      .then(data => allDestinations = data)
+      .catch(err => console.log('Fetch error: ', err))
   }
 }
 
