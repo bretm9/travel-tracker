18a966930e9a55b05626da7cde8018c1efe3e184
Bret Merritt <merrittbret9@gmail.com>
2020-09-20 16:12:16 -0600

    Add login function and add loginButton eventListener

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import domUpdates from './dom-updates';
 import Traveler from './traveler'
 import moment from 'moment'
 
-let traveler;
-
-let userID = 2;
+let traveler, userID;
 
 let selectedDestination;
 
 let userInputUsername = document.querySelector('#login-input-username');
 let userInputPassword = document.querySelector('#login-input-password');
+let loginView = document.querySelector('#login-view')
 let loginButton = document.querySelector('.login-button');
 let loginFooter = document.querySelector('#login-footer');
+let nav = document.querySelector('#nav');
+let mainView = document.querySelector('#main-view');
 let bookedTripsBody = document.querySelector('#booked-trips-body');
 let totalSpentBody = document.querySelector('#total-spent-body');
 let destinationsBody = document.querySelector('#desinations-body');
@@ -24,12 +25,14 @@ let travelerQuantitySelection = document.querySelector('#traveler-quantity-selec
 let durationSelection = document.querySelector('#duration-selection');
 let errorSection = document.querySelector('#error-section');
 
-
 let nodes = {
   userInputUsername, 
   userInputPassword,
+  loginView,
   loginButton,
   loginFooter,
+  nav,
+  mainView,
   bookedTripsBody,
   totalSpentBody,
   destinationsBody,
@@ -41,13 +44,19 @@ let nodes = {
   errorSection
 }
 
-window.addEventListener('load', (event) => {
-// loginButton.addEventListener('click', () => {
-  // if (login(userInputUsername.value, userInputPassword)) {
+// Accessibility branch eventListener:
+// window.addEventListener('load', (event) => {
+  // userID = 2;
+//   fetcher.getTravelerDestinations(traveler, userID, bookedTripsBody);
+// }
+
+loginButton.addEventListener('click', () => {
+  if (login(userInputUsername.value, userInputPassword.value)) {
     fetcher.getTravelerDestinations(traveler, userID, bookedTripsBody);
-    // domUpdates.toggleMainView(loginNode, navNode, mainNode);
-    // domUpdates.renderUserTrips(traveler, allDestinations, tripsNode);
-  // }
+    domUpdates.toggleMainView();
+  } else {
+    loginFooter.classList.remove('hidden');
+  }
 });
 
 window.addEventListener('click', selectDestination);
@@ -74,23 +83,23 @@ function inputCorrectFormat(input) {
   return moment(input.value, 'YYYY/MM/DD',true).isValid()
 }
 
-// function login(username, password) {
-//   let splitUsername = username.split('traveler');
-//   if (splitUsername[0] === `traveler` 
-//     && splitUsername[1] > 0 
-//     && splitUsername[1] <= 50 
-//     && password === 'travel2020'
-//   ) {
-//     this.toggleMainView();
-//     return splitUsername[1];
-//   } else if (splitUsername[0] === 'agent' 
-//       && password === 'travel2020'
-//     ) {
-//     this.toggleMainView();
-//     return splitUsername[0];
-//   }
-//   loginFooter.classList.remove('hidden');
-//   return false;
-// }
+function login(username, password) {
+  let splitUsername = username.split('traveler');
+  console.log(splitUsername)
+  if (splitUsername[0] === ''
+    && parseInt(splitUsername[1]) > 0 
+    && parseInt(splitUsername[1]) <= 50 
+    && password === 'travel2020'
+  ) {
+    userID = parseInt(splitUsername[1]);
+    return true;
+  // } else if (splitUsername[0] === 'agent' 
+  //     && password === 'travel2020'
+  //   ) {
+  //   // return true;
+  }
+  loginFooter.classList.remove('hidden');
+  return false;
+}
 
 export default nodes;
