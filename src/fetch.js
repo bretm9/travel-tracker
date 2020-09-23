b591b0a27669d286ab437fd015ab5d495fa0360f
Bret Merritt <merrittbret9@gmail.com>
2020-09-22 18:45:16 -0600

    Generate instantiations in fetch, refactor getUserTrips to getAllTrips

--- src/fetch.js
+++ src/fetch.js
@@ -1,15 +1,23 @@
 import Traveler from './traveler'
 import domUpdates from './dom-updates'
+import TripsRepo from './trips-repo';
+import Trip from './trip'
+import DestinationsRepo from './destinations-repo';
+import Destination from './destination'
 
 let allDestinations;
+
+let allTrips;
 
 let fetcher = {
 
   getTravelerDestinations(traveler, userID) {
-    // this.getAllDestinations(allDestinations)
     fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations')
       .then(response => response.json())
-      .then(data => allDestinations = data.destinations)
+      .then(data => {
+        allDestinations = new DestinationsRepo(data.destinations);
+        allDestinations.generateInstantiations(Destination)
+      })
       .then(() => {
         this.getTraveler(traveler, userID);
       })
@@ -21,59 +29,46 @@ let fetcher = {
       .then(response => response.json())
       .then(data => {
         traveler = new Traveler(data);
-        this.getUserTrips(traveler);
+        this.getAllTrips(traveler);
       })
       .catch(err => console.log('Fetch error: ', err))
   },
 
-  getUserTrips(traveler) {
-    let allTrips;
+  getAllTrips(traveler) {
+    let allTripsData;
     fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips')
       .then(response => response.json())
       .then(data => {
-        allTrips = data;
+        allTripsData = data;
+        allTrips = new TripsRepo(allTripsData.trips);
+        allTrips.generateInstantiations(Trip);
+        allTrips.instantiations.forEach(trip => {
+          trip.getTripCost(allDestinations.instantiations);
+        })
+      })
+      .then(() =>{
+        traveler.getUserTrips(allTrips.instantiations, allDestinations.instantiations);
       })
       .then(() => {
-        let userTrips = allTrips.trips.filter(trip => {
-          return trip.userID === traveler.id;
-        });
-        traveler.allDestinations = allDestinations;
-        traveler.trips = userTrips;
-        this.renderAfterFetch(traveler);
+        this.renderAfterFetch(traveler)
       })
       .catch(err => console.log('Fetch error: ', err))
   },
 
   renderAfterFetch(traveler) {
-    domUpdates.renderUserTrips(traveler, traveler.allDestinations);
+    domUpdates.renderUserTrips(traveler, allDestinations);
     domUpdates.renderTotalSpentThisYear(traveler);
     domUpdates.renderDesinationCards(traveler);
   },
 
   postRequestedTrip(traveler, userID, nodes, selectedDestination) {
-    let allTrips;
     fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips')
       .then(response => response.json())
       .then(data => {
         allTrips = data;
       })
       .then(() => {
-        let init = {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            id: (allTrips.trips.length + 1), 
-            userID, 
-            destinationID: selectedDestination, 
-            travelers: parseInt(nodes.travelerQuantitySelection.value),
-            date: nodes.startDateInput.value,
-            duration: parseInt(nodes.durationSelection.value), 
-            status: 'pending', 
-            suggestedActivities: []
-          })
-        };
+        let init = this.generatePostInit(userID, nodes, selectedDestination);
         fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips', init)
           .then(response => response.json())
           .then(() => {
